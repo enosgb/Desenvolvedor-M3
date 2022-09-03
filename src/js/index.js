18c2a4494131ee1345bb737f4e97da0fc87a337c
Enos Gabriel <enosgb55@gmail.com>
2022-09-03 18:31:33 -0300

    consumindo api products

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
+const elemento = document.getElementById("produtos");
+
 fetch("http://localhost:5000/products")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    data.slice(0,9).forEach((item, i) => {
+      const template = `
+        <div class="produto">
+            <img src="${item.image}" />
+            <h3 class="produto_title">${item.name}</h3>
+            <h3 class="produto_preco">R$ ${item.price.toFixed(2)}</h3>
+            <h3 class="produto_parcelamento">até ${item.parcelamento[0]}x de R$${item.parcelamento[1].toFixed(2)}</h3>
+            <button class="btn_comprar">Comprar</button>
+        </div>`;
+      elemento.innerHTML += template;
+    });
+    elemento.innerHTML += ``
   });
-
-  
