94dd5de7f741ac136a5627886c23892837df0f4d
Enos Gabriel <enosgb55@gmail.com>
2022-09-04 15:59:03 -0300

    habilitando botão e checkbox

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,65 @@
-const elemento = document.getElementById("produtos");
+function getProducts() {
+  const elemento = document.getElementById("produtos");
 
-fetch("http://localhost:5000/products")
-  .then((response) => response.json())
-  .then((data) => {
-    data.slice(0,9).forEach((item, i) => {
-      const template = `
-        <div class="produto">
-            <img src="${item.image}" />
-            <h3 class="produto_title">${item.name}</h3>
-            <h3 class="produto_preco">R$ ${item.price.toFixed(2)}</h3>
-            <h3 class="produto_parcelamento">até ${item.parcelamento[0]}x de R$${item.parcelamento[1].toFixed(2)}</h3>
-            <button class="btn_comprar">Comprar</button>
-        </div>`;
-      elemento.innerHTML += template;
+  fetch("http://localhost:5000/products")
+    .then((response) => response.json())
+    .then((data) => {
+      let produtos = data;
+      produtos.slice(0, 9).forEach((item, i) => {
+        const template = `
+          <div class="produto">
+              <img src="${item.image}" />
+              <h3 class="produto_title">${item.name}</h3>
+              <h3 class="produto_preco">R$ ${item.price.toFixed(2)}</h3>
+              <h3 class="produto_parcelamento">até ${
+                item.parcelamento[0]
+              }x de R$${item.parcelamento[1].toFixed(2)}</h3>
+              <button id=${item.id} class="btn_comprar">Comprar</button>
+          </div>`;
+        elemento.innerHTML += template;
+      });
     });
-    elemento.innerHTML += ``
+}
+
+function btn_sacola(){
+  const btn = document.querySelector('.btn_sacola');
+  btn.onclick = function(){
+    console.log('btn sacola clicked')
+  }
+}
+
+function btnComprar() {
+  const btn = document.querySelectorAll(".btn_comprar");
+  btn.forEach((btn, i) => {
+    btn.onclick = function (event) {
+      console.log(event.target.id);
+    };
   });
+}
+
+function checkbox() {
+  const checkbox = document.querySelectorAll(".checkbox");
+  checkbox.forEach((checkbox, i) => {
+    checkbox.onchange = function (event) {
+      console.log(event.target.labels[0].innerText.trim());
+    };
+  });
+}
+
+function selectTamanhos() {
+  const tamanho = document.querySelectorAll(".caixa_tamanhos");
+  tamanho.forEach((tamanho, i) => {
+    tamanho.onclick = function (event) {
+      console.log(event.target.textContent.trim());
+    };
+  });
+}
+
+window.onload = function () {
+  btn_sacola()
+  btnComprar();
+  checkbox();
+  selectTamanhos();
+};
+
+getProducts();
