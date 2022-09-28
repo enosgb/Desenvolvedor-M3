ecc298bde0f2a8b3b7064531154fd61e608f7f46
Enos Gabriel <enosgb55@gmail.com>
2022-09-28 10:13:07 -0300

    validate screen

--- src/js/app/controllers/ProductController.js
+++ src/js/app/controllers/ProductController.js
@@ -16,17 +16,27 @@ export class ProductController {
     this._productsView = new ProductsView($("#products"));
     this._cartView = new CartView(this._cart);
     this._shoppingList = new ShoppingList();
-    this._cartController = new CartController(this._cartView,this._shoppingList);
+    this._cartController = new CartController(
+      this._cartView,
+      this._shoppingList
+    );
+    this._lenProducts = this._validateScreen();
   }
 
-  _getProducts() {
+  _validateScreen() {
+    if (screen.width > 600 && screen.width < 1300) return 6;
+    else if (screen.width <= 600) return 4;
+    else return 9;
+  }
+
+  _getProducts(lenProducts) {
     fetch("http://localhost:5000/products")
       .then((response) => response.json())
       .then((products) => {
         this._productsList.add(products);
         this._sidebarController = new SidebarController(products);
         this._sidebarController.loadSidebar();
-        this._productsView.update(this._productsList.products.slice(0, 9));
+        this._productsView.update(this._productsList.products.slice(0, lenProducts));
         this._getMoreProducts(this._productsList.products);
       });
   }
@@ -56,19 +66,17 @@ export class ProductController {
           if (p.id == id) {
             ShoppingList.add(p);
             cartView.update(ShoppingList.purchases);
-            cartView.countItemsCart(ShoppingList.purchases.length, btnBag); 
-            id = -1
+            cartView.countItemsCart(ShoppingList.purchases.length, btnBag);
+            id = -1;
           }
         });
       }
     });
   }
 
- 
-
   carregarProdutos() {
-    this._getProducts();
+    this._getProducts(this._lenProducts);
     this._purchase();
-    this._cartController.loadCart()
+    this._cartController.loadCart();
   }
 }
