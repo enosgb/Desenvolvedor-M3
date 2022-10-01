71082e5ad02d162adc6cab942e00c23056101d58
Enos Gabriel <enosgb55@gmail.com>
2022-10-01 12:23:56 -0300

    mobile sidebar

--- src/js/app/controllers/MobileController.js
+++ src/js/app/controllers/MobileController.js
@@ -7,6 +7,12 @@ export class MobileController {
     this._mb_products = $(".content__container");
     this._mb_footer = $(".footer");
     this._sidebar = $(".content__sidebar");
+    this._mb_div_colors = $("#mb_div_colors");
+    this._mb_div_sizes = $("#div_mb_sizes");
+    this._mb_div_prices = $("#div_mb_prices");
+    this._content_div_colors = $(".content__sidebar__checkboxes_colors");
+    this._content_div_sizes = $(".content__sidebar__sizes");
+    this._content_div_prices = $(".content__sidebar__prices");
   }
 
   mobile() {
@@ -17,6 +23,12 @@ export class MobileController {
     let mb_products = this._mb_products;
     let mb_footer = this._mb_footer;
     let sidebar = this._sidebar;
+    let mb_div_colors = this._mb_div_colors;
+    let mb_div_sizes = this._mb_div_sizes;
+    let mb_div_prices = this._mb_div_prices;
+    let content_div_colors = this._content_div_colors;
+    let content_div_sizes = this._content_div_sizes;
+    let content_div_prices = this._content_div_prices;
 
     function hideAndUnhideScreen() {
       if (mb_footer.classList.contains("mobile_hide")) {
@@ -29,7 +41,7 @@ export class MobileController {
       } else {
         mb_title.classList.remove("content__sidebar_title");
         mb_btnFilters.remove("content__container__mobile_buttons");
-        mb_title.classList.add("mobile_hide");
+        mb_title.style.add("mobile_hide");
         mb_btnFilters.classList.add("mobile_hide");
         mb_products.classList.add("mobile_hide");
         mb_footer.classList.add("mobile_hide");
@@ -51,12 +63,22 @@ export class MobileController {
     function mobileButtons() {
       mobile_buttons.addEventListener("click", function (event) {
         event.preventDefault();
+        expandDivMobile(content_div_colors, mb_div_colors, "block");
+        expandDivMobile(content_div_sizes, mb_div_sizes, "flex");
+        expandDivMobile(content_div_prices, mb_div_prices, "block");
         hideAndUnhideScreen();
         if (event.target.id == "btn_mobile_filter") {
           sidebar.style.display = "block";
         } else {
           hideAndUnhideSelectMobile();
         }
+      });
+    }
+
+    function expandDivMobile(content, div, display) {
+      div.addEventListener("click", function (event) {
+        if (content.style.display == "none") content.style.display = display;
+        else content.style.display = "none";
       });
     }
 
